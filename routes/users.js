cc151531b1d879ab1ad153f3b804bd231f8086ff
Faizal <91838778+FaizalLeviansyah@users.noreply.github.com>
2024-03-18 20:14:07 +0700

    second_commit

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import express from 'express';
-// import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
@@ -16,6 +16,17 @@ let users = [];
 // GET /users - find all users
 router.get('/', (req, res) => {
     res.send(users);
+});
+
+//Commit #2
+// POST /user - creates all user
+router.post('/', (req, res) => {
+    const user = req.body;
+    console.log(user)
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`${user.first_name} has been added to Database`);
 });
 
 export default router
